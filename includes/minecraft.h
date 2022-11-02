595daa06aa1830226c085b507c21d11c64baa6dd
Mathys Taquet <mtaquet@z4r10p1.42lyon.fr>
2022-11-02 11:51:46 +0100

    temporary neighbour fix

--- includes/minecraft.h
+++ includes/minecraft.h
@@ -191,27 +191,27 @@ void Minecraft::LoadChunks() {
                 newChunk->Bind();*/
                 chunks.push_back(newChunk);     //if needed push_back fist the most important chunk or create a priority list
                 loadedChunks[x][z] = newChunk;
-            }
-           /* if (loadedChunks[x][z]->status != CHUNK_FULLY_LOADED) {
-                if (loadedChunks[x][z]->neighbour[CHUNK_FRONT_SIDE] == 0 && x > 0 && loadedChunks[x - 1][z]) {
+            }/*
+            if (loadedChunks[x][z]->status == CHUNK_LOADED && loadedChunks[x][z]->threadStatus == CHUNK_NOT_PROCESSING) {
+                if (loadedChunks[x][z]->neighbour[CHUNK_FRONT_SIDE] == 0 && x > 0 && loadedChunks[x - 1][z] && loadedChunks[x - 1][z]->status == CHUNK_LOADED && loadedChunks[x - 1][z]->threadStatus == CHUNK_NOT_PROCESSING) {
                     loadedChunks[x][z]->addNeighbour(loadedChunks[x - 1][z], CHUNK_FRONT_SIDE);
                     if (loadedChunks[x - 1][z]->neighbour[CHUNK_BACK_SIDE] == 0)
                         loadedChunks[x - 1][z]->addNeighbour(loadedChunks[x][z], CHUNK_BACK_SIDE);
                 }
                 
-                if (loadedChunks[x][z]->neighbour[CHUNK_BACK_SIDE] == 0 && x < (maxChunk - 1) && loadedChunks[x + 1][z]) {
+                if (loadedChunks[x][z]->neighbour[CHUNK_BACK_SIDE] == 0 && x < (maxChunk - 1) && loadedChunks[x + 1][z] && loadedChunks[x + 1][z]->status == CHUNK_LOADED && loadedChunks[x + 1][z]->threadStatus == CHUNK_NOT_PROCESSING) {
                     loadedChunks[x][z]->addNeighbour(loadedChunks[x + 1][z], CHUNK_BACK_SIDE);
                     if (loadedChunks[x + 1][z]->neighbour[CHUNK_FRONT_SIDE] == 0)
                         loadedChunks[x + 1][z]->addNeighbour(loadedChunks[x][z], CHUNK_FRONT_SIDE);
                 }
 
-                if (loadedChunks[x][z]->neighbour[CHUNK_LEFT_SIDE] == 0 && z > 0 && loadedChunks[x][z - 1]) {
+                if (loadedChunks[x][z]->neighbour[CHUNK_LEFT_SIDE] == 0 && z > 0 && loadedChunks[x][z - 1] && loadedChunks[x][z - 1]->status == CHUNK_LOADED && loadedChunks[x][z - 1]->threadStatus == CHUNK_NOT_PROCESSING) {
                     loadedChunks[x][z]->addNeighbour(loadedChunks[x][z - 1], CHUNK_LEFT_SIDE);
                     if (loadedChunks[x][z - 1]->neighbour[CHUNK_RIGHT_SIDE] == 0)
                         loadedChunks[x][z - 1]->addNeighbour(loadedChunks[x][z], CHUNK_RIGHT_SIDE);
                 }
 
-                if (loadedChunks[x][z]->neighbour[CHUNK_RIGHT_SIDE] == 0 && z < (maxChunk - 1) && loadedChunks[x][z + 1]) {
+                if (loadedChunks[x][z]->neighbour[CHUNK_RIGHT_SIDE] == 0 && z < (maxChunk - 1) && loadedChunks[x][z + 1] && loadedChunks[x][z + 1]->status == CHUNK_LOADED && loadedChunks[x][z + 1]->threadStatus == CHUNK_NOT_PROCESSING) {
                     loadedChunks[x][z]->addNeighbour(loadedChunks[x][z + 1], CHUNK_RIGHT_SIDE);
                     if (loadedChunks[x][z + 1]->neighbour[CHUNK_LEFT_SIDE] == 0)
                         loadedChunks[x][z + 1]->addNeighbour(loadedChunks[x][z], CHUNK_LEFT_SIDE);
