d12f4ecbfdd74c21e2ae503514699ecd50baaf56
matheme <maxence.theme@hoomano.com>
2022-11-19 18:50:30 +0100

    in progress

--- includes/entity.h
+++ includes/entity.h
@@ -32,7 +32,10 @@ public:
 		else {
 			position.y = ceil(position.y);
 		}
-		CheckCollision(movement);
+		if (!CheckCollision(movement)) {
+			position.x += movement.x;
+			position.z += movement.z;
+		}
 	}
 
 	float updateFromGravity(float frequence) {
@@ -82,12 +85,53 @@ private:
 		std::cout << "x: " << vect.x << " y : " << vect.y << " z : " << vect.z << std::endl;
 	}
 
-	bool CheckCollision(glm::vec3 movement) {
-		glm::vec3 ratio = glm::normalize(movement);
-		
-		if (glm::all(glm::isnan(ratio))) return (false);
-		printVect(ratio);
+	/// CALCULATE COLLISION
 
+	glm::vec3 _calculateStep(glm::vec3 movement, float minStepPrecision) {
+		glm::vec3 step = glm::abs(movement);
+		while (step.x > minStepPrecision || step.y > minStepPrecision || step.z > minStepPrecision)
+			step = glm::vec3(step.x / 2, step.y / 2, step.z / 2);
+		return (step);
+	}
+
+	bool CheckCollision(glm::vec3 movement) {
+		glm::vec3 step;					// correspond au pas du raycasting
+		glm::uvec3 sens;				// correspond au sens de direction -1 ou 1
+		glm::vec3 newPosition;
+		glm::vec3 intNewPosition;
+		glm::vec3 collideSize;
+
+
+		step = _calculateStep(movement, 0.5f);
+		sens = glm::uvec3(movement.x > 0 ? 1 : -1, movement.y > 0 ? 1 : -1, movement.z > 0 ? 1 : -1);
+		printVect(step);
+
+		float istep = 0.0f;
+		float igoal = 0;
+		if (step.x > step.y && step.x > step.z) {
+			istep = step.x;
+			igoal = glm::abs(movement.x);
+		}
+		else if (step.y > step.x && step.y > step.z) {
+			istep = step.y;
+			igoal = glm::abs(movement.y);
+		}
+		else {
+			istep = step.z;
+			igoal = glm::abs(movement.z);
+		}
+
+		step = glm::vec3(step.x * sens.x, step.y * sens.y, step.z * sens.z);
+		
+		
+		//collideSize = glm::vec3(step.x > 0 ? size.x / 2 : -size.x / 2, size.y, step.z > 0 ? size.z / 2 : -size.z);
+		/*for (int i = 1; istep * i < igoal; i++) {
+			newPosition = glm::vec3(position.x + collideSize.x + (step.x * i), position.y + collideSize.y + (step.y * i), position.z + collideSize.z + (step.z * i));
+			intNewPosition = glm::vec3(step.x > 0 ? glm::floor(newPosition.x) : glm::ceil(newPosition.x), step.y > 0 ? glm::floor(newPosition.y) : glm::ceil(newPosition.y), step.z > 0 ? glm::floor(newPosition.z) : glm::ceil(newPosition.z));
+			if (GetCubeAt(intNewPosition.x, intNewPosition.y, intNewPosition.z)) {
+				return (true);
+			}
+		}*/
 		return (false);
 	}
 };
