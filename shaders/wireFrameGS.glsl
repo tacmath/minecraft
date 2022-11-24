ee65a88d5ab1606a5b65036ac7332378915a2f09
Mathys <tacmath@gmail.com>
2022-11-24 20:15:38 +0100

    small change

--- shaders/wireFrameGS.glsl
+++ shaders/wireFrameGS.glsl
@@ -1,6 +1,7 @@
 #version 400 core
 layout (triangles) in;
 layout (line_strip, max_vertices = 3) out;
+//layout (triangle_strip, max_vertices = 3) out;
 
 in vec3 texCoord[];
 in float luminosity[];
