e929f17611e7844b16fece8f8f213230ad5d0a6b
Mathys <tacmath@gmail.com>
2023-01-02 17:48:42 +0100

    small change

--- includes/sun.h
+++ includes/sun.h
@@ -7,7 +7,8 @@
 #include "quad.h"
 
 class Sun {
-	glm::mat4	model;
+	glm::mat4	sunModel; //maybe have a shader for each
+	glm::mat4	moonModel;
 	glm::vec3	position;
 	Shader		shader;
 	Texture		texture;
@@ -19,7 +20,8 @@ public:
 	Sun() {
 		ticks = 0;
 		position = glm::vec3(0.0f);
-		model = glm::mat4(1.0f);
+		sunModel = glm::mat4(1.0f);
+		moonModel = glm::mat4(1.0f);
 		texture.LoadArray({"texture/sun.png", "texture/moon.png"}, 2);
 		quad.Init(glm::vec2(-0.5f), glm::vec2(1.0f));
 		shader.Load("shaders/sunVS.glsl", "shaders/sunFS.glsl");
@@ -35,21 +37,23 @@ public:
 	void Draw() {
 		shader.Activate();
 		shader.setInt("phase", 0);
-		shader.setMat4("model", glm::translate(glm::mat4(1.0f), position) * model);
+		shader.setMat4("model", sunModel);
 		quad.Render();
 
 		shader.setInt("phase", 1);
-		shader.setMat4("model", glm::translate(glm::mat4(1.0f), -position) * model);
+		shader.setMat4("model", moonModel);
 		quad.Render();
 	}
 
 	void Update() {
 		float angle;
 
+
 		angle = glm::radians(ticks / 10.0f);
 		position.z = -cos(angle);
 		position.y = sin(angle);
-		model = glm::rotate(glm::mat4(1.0f), angle, glm::vec3(1.0f, 0.0f, 0.0f));
+		sunModel = glm::rotate(glm::translate(glm::mat4(1.0f), position), angle, glm::vec3(1.0f, 0.0f, 0.0f));
+		moonModel = glm::rotate(glm::translate(glm::mat4(1.0f), -position), angle, glm::vec3(1.0f, 0.0f, 0.0f));
 	}
 
 	void SetView(glm::mat4 &view) {
