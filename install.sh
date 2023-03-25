8f1cced7ad198a18888ae8156b1b57322ac45910
tacmath <tacmathys@gmail.com>
2023-03-25 15:33:30 +0100

    change install.sh

--- install.sh
+++ install.sh
@@ -4,58 +4,48 @@ mkdir -p ./libraries
 mkdir -p ./libraries/lib
 mkdir -p ./libraries/include
 
+git submodule init
+git submodule update
 
 path=./libraries/include/glad/khrplatform.h
-if [ -e $path ]
+if [  ! -e $path ]
 then
-    echo '' > /dev/null
-else
-    echo 'installing khrplatform'
+    printf '\ninstalling khrplatform\n'
     mkdir -p ./libraries/include/glad
     curl -k https://raw.githubusercontent.com/matheme42/glad/master/khrplatform.h > ./libraries/include/glad/khrplatform.h
 
 fi
 
-
-
 path=./libraries/include/stb_image.h
-if [ -e $path ]
+if [  ! -e $path ]
 then
-    echo '' > /dev/null
-else
-    echo 'installing stb_image.h'
+    printf '\ninstalling stb_image.h\n'
     curl https://raw.githubusercontent.com/nothings/stb/master/stb_image.h > ./libraries/include/stb_image.h
 
 fi
 
 path=./libraries/include/glad/glad.h
-if [ -e $path ]
+if [  ! -e $path ]
 then
-    echo '' > /dev/null
-else
-    echo 'installing glad.h'
+    printf '\ninstalling glad.h\n'
     mkdir -p ./libraries/include/glad
     curl https://raw.githubusercontent.com/matheme42/glad/master/glad.h > ./libraries/include/glad/glad.h
 
 fi
 
 path=./glad.c
-if [ -e $path ]
+if [  ! -e $path ]
 then
-    echo '' > /dev/null
-else
-    echo 'installing glad.c'
+    printf '\ninstalling glad.c\n'
     curl https://raw.githubusercontent.com/matheme42/glad/master/glad.c > ./glad.c
 
 fi
 
 
 path=./libraries/include/glm
-if [ -e $path ]
+if [ ! -e $path ]
 then
-    echo '' > /dev/null
-else
-    echo 'installing glm'
+    printf '\ninstalling glm\n'
     git clone https://github.com/g-truc/glm.git ./libraries/include/.tmp
     mv -f ./libraries/include/.tmp/glm ./libraries/include/
     rm -rf ./libraries/include/.tmp
@@ -63,11 +53,9 @@ else
 fi
 
 path=./libraries/include/GLFW
-if [ -e $path ]
+if [ ! -e  $path ]
 then
-    echo '' > /dev/null
-else
-    echo 'installing glfw'
+    printf '\ninstalling glfw\n'
     git clone https://github.com/glfw/glfw.git ./.tmp
     mv -f ./.tmp/include/* ./libraries/include/
     rm -rf ./.tmp
@@ -76,10 +64,8 @@ fi
 
 
 path=./libraries/include/freetype
-if [ -e $path ]
+if [  ! -e $path ]
 then
-    echo '' > /dev/null
-else
     echo 'installing freetype'
     git clone https://github.com/freetype/freetype.git ./.tmp
     mv -f ./.tmp/include/* ./libraries/include/
@@ -88,10 +74,8 @@ else
 fi
 
 path=~/.dep
-if [ -e $path ]
+if [  ! -e $path ]
 then
-    echo '' > /dev/null
-else
     echo 'instaling lib'
     mkdir -p ~/.dep
     cd ~/.dep
@@ -103,21 +87,38 @@ else
     cd -
 fi
 
-path=./libraries/lib/glfw3lib
-if [ -e $path ]
+path=~/.dep/usr/lib/*/libglfw.so.3
+if [  ! -e $path ]
 then
-    echo '' > /dev/null
-else
+    echo 'instaling libglfw'
+    cd ~/.dep
+    apt download libglfw3
+    dpkg -x libglfw* .
+    cd -
+fi
+
+path=~/.dep/usr/lib/*/libfreetype.so.6
+if [  ! -e $path ]
+then
+    echo 'instaling libfreetype'
+    cd ~/.dep
+    apt download libfreetype6
+    dpkg -x libfreetype* .
+    cd -
+fi
+
+
+path=./libraries/lib/glfw3lib
+if [  ! -e $path ]
+then
     echo 'copy libglfw'
     mkdir -p ./libraries/lib/glfw3lib
     cp ~/.dep/usr/lib/*/libglfw* ./libraries/lib/glfw3lib
 fi
 
 path=./libraries/lib/freetypelib
-if [ -e $path ]
+if [  ! -e $path ]
 then
-    echo '' > /dev/null
-else
     echo 'copy libfreetype'
     mkdir -p ./libraries/lib/freetypelib
     cp ~/.dep/usr/lib/*/libfreetype* ./libraries/lib/freetypelib
